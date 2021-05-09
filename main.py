a80727800da556a7b4eb4c0e102862354ac78c90
mtk12 <taha.khan@dastgyr.com>
2021-05-09 16:05:34 +0500

    Flask app made

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/second')
+def second():
+    return render_template("second_page.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port= 4000)
